442d4700a493a14d5bd1a0681a8e14ef833c0d88
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-04 10:59:09 -0800

    add code to return vendor id

--- Marketplace/Models/Vendor.cs
+++ Marketplace/Models/Vendor.cs
@@ -4,9 +4,17 @@ namespace Marketplace.Models
   public class Vendor
   {
     public string Name {get; set;}
+    private static List<Vendor> _instances = new List<Vendor>{};
+    public int Id {get;}
     public Vendor(string name)
     {
       Name = name;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
