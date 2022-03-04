0a5e49bda7dcb09d009c830f73441626d08154da
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-04 09:49:08 -0800

    add code to return empty list, create clear all method

--- Marketplace.Tests/ModelTests/OrderTests.cs
+++ Marketplace.Tests/ModelTests/OrderTests.cs
@@ -6,8 +6,12 @@ using System.Collections.Generic;
 namespace Marketplace.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
--- Marketplace/Models/Order.cs
+++ Marketplace/Models/Order.cs
@@ -9,14 +9,22 @@ namespace Marketplace.Models
     public string Description {get;set;}
     public int Price {get;set;}
     public DateTime Date{get;set;}
-    
-
+    private static List<Order> _instances = new List<Order>{};
     public Order(string title, string description, int price, DateTime date)
     {
       Title = title;
       Description = description;
       Price = price;
       Date = date;
+      _instances.Add(this);
+    }
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
