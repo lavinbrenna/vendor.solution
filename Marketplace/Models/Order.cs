9ab427afd277d398a017d2837659a63b02ee3d71
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-04 10:04:10 -0800

    add code to find order by id

--- Marketplace/Models/Order.cs
+++ Marketplace/Models/Order.cs
@@ -24,6 +24,10 @@ namespace Marketplace.Models
     {
       return _instances;
     }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
     public static void ClearAll()
     {
       _instances.Clear();
