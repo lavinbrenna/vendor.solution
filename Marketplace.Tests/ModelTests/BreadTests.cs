0182a774051208f31effedee86f2a6adce21fed4
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-04 15:32:07 -0800

    incorporate bread class and tests from previous project

--- Marketplace.Tests/ModelTests/BreadTests.cs
+++ Marketplace.Tests/ModelTests/BreadTests.cs
@@ -6,13 +6,74 @@ using Marketplace.Models;
 namespace Marketplace.Tests
 {
   [TestClass]
-  public class BreadTests
+
+  public class BreadTests : IDisposable
   {
+    public void Dispose()
+    {
+      Bread.ClearAll();
+    }
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(1, "Shokupan");
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+    [TestMethod]
+    public void GetLoafCount_ReturnCountOfLoavesOfBread_Int()
+    {
+      Bread newBread = new Bread(1, "Shokupan");
+      int loafNumber = newBread.LoafCount;
+      Assert.AreEqual(loafNumber, 1);
+    }
+    [TestMethod]
+    public void GetLoafPrice_ReturnPriceOfLoaves_Int()
+    {
+      Bread newBread = new Bread(1, "Shokupan");
+      Bread multipleLoaves = new Bread(6, "Brioche");
+      int loafNumber = newBread.LoafCount;
+      int loavesNumber = multipleLoaves.LoafCount;
+      int breadPrice = Bread.GetLoafPrice(loafNumber);
+      int loavesPrice = Bread.GetLoafPrice(loavesNumber);
+      Assert.AreEqual(breadPrice, 5);
+      Assert.AreEqual(loavesPrice, 20);
+    }
+    [TestMethod]
+    public void GetBreadType_ReturnBreadType_String()
+    {
+      Bread newBread = new Bread(1, "Shokupan");
+      string breadStyle = newBread.BreadType;
+      Assert.AreEqual(breadStyle, "Shokupan");
+    }
+    [TestMethod]
+    public void GetAllBread_ReturnsEmptyBreadList_BreadList()
+    {
+      List<Bread> newList = new List<Bread> {};
+      List<Bread> result = Bread.GetAllBread();
+      foreach(Bread thisBread in result)
+      {
+        Console.WriteLine("Output from empty list GetAllBread test: " + thisBread.BreadType);
+      }
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void GetAllBread_ReturnsBreadLoaves_BreadList()
+    {
+      string breadStyle1 = "Brioche";
+      int loafCount1 = 1;
+      string breadStyle2 = "Challah";
+      int loafCount2 = 2;
+      Bread newBread1 = new Bread(loafCount1, breadStyle1);
+      Bread newBread2 = new Bread(loafCount2, breadStyle2);
+      List<Bread> newList = new List<Bread>{
+        newBread1, newBread2
+      };
+      List<Bread> result = Bread.GetAllBread();
+      foreach( Bread thisBread in result)
+      {
+        Console.WriteLine("Output from second GetAllBread test: " + thisBread.BreadType);
+      }
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
--- Marketplace/Models/Bread.cs
+++ Marketplace/Models/Bread.cs
@@ -4,6 +4,46 @@ namespace Marketplace.Models
 {
   public class Bread
   {
-    
+    public int LoafCount{get;set;}
+    public string BreadType{get;set;}
+
+    private static List<Bread> _instances = new List<Bread>{};
+
+    public Bread(int loafCount, string breadType)
+    {
+      LoafCount = loafCount;
+      BreadType = breadType;
+      _instances.Add(this);
+    }
+
+    public static int GetLoafPrice(int loafCount)
+    {
+      int loafPrice = 0;
+      List<int>loafList = new List<int>{};
+      for(int i = 0; i <= loafCount; i++)
+      {
+        loafList.Add(i);
+      }
+      foreach(int loaf in loafList)
+      {
+        if(loaf % 3 == 0)
+        {
+          loafPrice += 0;
+        }
+        else
+        {
+          loafPrice += 5;
+        }
+      }
+      return loafPrice;
+    }
+    public static List<Bread> GetAllBread()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
