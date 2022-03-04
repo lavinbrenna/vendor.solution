46362765dc01f76e1d9ffa2e2478d62ee53df32e
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-04 09:55:16 -0800

    add test to return list of orders

--- Marketplace.Tests/ModelTests/OrderTests.cs
+++ Marketplace.Tests/ModelTests/OrderTests.cs
@@ -74,6 +74,29 @@ namespace Marketplace.Tests
       }
       CollectionAssert.AreEqual(newList, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      string title01 = "title";
+      string title02 = "title";
+      string description01 ="description";
+      string description02="description";
+      int price01 = 25;
+      int price02 = 25;
+      DateTime date01 = new DateTime(2022, 03,04);
+      DateTime date02 = new DateTime(2022, 03, 04);
+      Order newOrder1 = new Order(title01, description01, price01, date01);
+      Order newOrder2 = new Order(title02, description02, price02, date02);
+      List<Order> newList = new List<Order>{
+        newOrder1, newOrder2
+      };
+      List<Order> result = Order.GetAll();
+      foreach(Order thisOrder in result)
+      {
+        Console.WriteLine("Output from getAll Test: "+ thisOrder.Title);
+      }
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 
 }
