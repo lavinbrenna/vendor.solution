159b31cc00bc1e12d6d5a4eab6b28f3361c1e9ba
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-04 10:00:46 -0800

    add code to add order id

--- Marketplace/Models/Order.cs
+++ Marketplace/Models/Order.cs
@@ -10,6 +10,7 @@ namespace Marketplace.Models
     public int Price {get;set;}
     public DateTime Date{get;set;}
     private static List<Order> _instances = new List<Order>{};
+    public int Id{get;}
     public Order(string title, string description, int price, DateTime date)
     {
       Title = title;
@@ -17,6 +18,7 @@ namespace Marketplace.Models
       Price = price;
       Date = date;
       _instances.Add(this);
+      Id = _instances.Count;
     }
     public static List<Order> GetAll()
     {
