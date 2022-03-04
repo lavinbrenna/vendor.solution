f426ae3e41a463d5a8f18a8e9f1692c825f5b8d0
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-04 12:50:07 -0800

    add orders controller, shared layout

--- Marketplace/Controllers/OrdersControllers.cs
+++ Marketplace/Controllers/OrdersControllers.cs
@@ -6,5 +6,27 @@ namespace MarketPlace.Controllers
 {
 public class OrdersController : Controller
 {
+  [HttpGet("/vendors/{vendorId}/orders/new")]
+  public ActionResult New(int vendorId)
+  {
+    Vendor vendor = Vendor.Find(vendorId);
+    return View(vendor);
+  }
+  [HttpGet("/vendors/{vendorId}/orders/{id}")]
+  public ActionResult Show(int vendorId, int orderId)
+  {
+    Order order = Order.Find(orderId);
+    Vendor vendor = Vendor.Find(vendorId);
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    model.Add("order", order);
+    model.Add("vendor", vendor);
+    return View(model);
+  }
+  [HttpGet("/orders/delete")]
+  public ActionResult DeleteAll()
+  {
+    Order.ClearAll();
+    return View();
+  }
 }
 }
